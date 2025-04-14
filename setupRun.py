24943d4c20ad99b1632a34f923dc527782c6b329
Rishith44 <rishithreddy44@gmail.com>
2025-04-14 16:03:06 -0400

    setup run update
    ---

--- setupRun.py
+++ setupRun.py
@@ -1,23 +1,27 @@
 print("Installing required packages for Ice'nSpice Restaurant...")
 
 import os
-os.system("pip install customtkinter")
+os.system("pip3 install customtkinter")
 print("✓ Installed customtkinter")
 
-os.system("pip install pillow")
+os.system("pip3 install pillow")
 print("✓ Installed pillow (PIL)")
 
-os.system("pip install mysql-connector-python")
+os.system("pip3 install mysql-connector-python")
 print("✓ Installed mysql-connector")
 
-os.system("pip install matplotlib")
+os.system("pip3 install matplotlib")
 print("✓ Installed matplotlib")
 
-os.system("pip install tkcalendar")
+os.system("pip3 install tkcalendar")
 print("✓ Installed tkcalendar")
 
-os.system("pip install bcrypt")
+os.system("pip3 install bcrypt")
 print("✓ Installed bcrypt")
+
+os.system("pip3 install watchdog")
+print("✓ Installed watchdog")
+
 
 print("\nAll packages installed successfully!")
 print("You can now run the application using 'python start.py'")
